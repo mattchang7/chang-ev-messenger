cc507a2d3d01a0412df3e6a10731d9bbf735b290
Matt Chang <mattchang@Matthews-MBP.lan>
2024-03-01 18:31:13 -0500

    fixed bug with minutes and hours on timestamp not showing 0 if single digit

--- src/util/timeConverter.ts
+++ src/util/timeConverter.ts
@@ -14,12 +14,12 @@ export const timeConverter = (UNIX_timestamp: number) => {
     "Nov",
     "Dec",
   ];
-  var year = a.getFullYear();
   var month = months[a.getMonth()];
   var date = a.getDate();
   var hour = a.getHours();
   var min = a.getMinutes();
-  var sec = a.getSeconds();
-  var time = `${month} ${date} at ${hour}:${min}`;
+  var time = `${month} ${date} at ${hour < 10 ? "0" + hour : hour}:${
+    min < 10 ? "0" + min : min
+  }`;
   return time;
 };
