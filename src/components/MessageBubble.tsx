bde4acfd608f7ec1d701e8b416f1cac512e95171
Matt Chang <mattchang@Matthews-MBP.lan>
2024-03-01 15:48:48 -0500

    responsive and form refactor

--- src/components/MessageBubble.tsx
+++ src/components/MessageBubble.tsx
@@ -28,7 +28,7 @@ export default function MessageBubble({ user, message }: MessageBubbleProps) {
   ) : (
     <div className="w-full flex flex-row justify-start p-1">
       <div className="flex flex-col text-xs">
-        {message.author}
+        {message.author[0].toUpperCase() + message.author.slice(1)}
         <div
           className="max-w-2xl w-fit bg-zinc-400 p-2 rounded-md break-words text-zinc-100 text-base"
           onClick={() => setOpen(!isOpen)}
--- src/components/MessageTerminal.tsx
+++ src/components/MessageTerminal.tsx
@@ -14,41 +14,36 @@ export default function MessageTerminal({ user }: MessageTerminalProps) {
   const [messageInput, setMessageInput] = useState("");
   const sendMessage = useMutation(api.messages.createMessage);
   const messages = useQuery(api.messages.getLastTenMessages);
-  const handleSubmit = (body: string, author: string) => {
-    sendMessage({
-      body,
-      author,
-    });
-    setMessageInput("");
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (messageInput !== "") {
+      sendMessage({
+        body: messageInput,
+        author: user,
+      });
+      setMessageInput("");
+    }
   };
   console.log(messages);
   return (
-    <div className="border  mt-5 h-5/6 w-1/2 flex flex-col">
+    <div className="border  mt-5 h-5/6 w-5/6 md:w-1/2 flex flex-col">
       <div className="border h-full flex flex-col p-2 overflow-scroll">
         {messages?.map((m) => (
           <MessageBubble key={m._id} message={m} user={user} />
         ))}
       </div>
-      <form className="border h-16 text-zinc-900 flex-row flex items-center px-2">
+      <form
+        className="border h-16 text-zinc-900 flex-row flex items-center px-2"
+        onSubmit={handleSubmit}
+      >
         <textarea
-          className="text-zinc-900 color-zinc-900 resize-none w-5/6 h-10 ring-0 focus:outline-none rounded-full px-3 py-2"
+          className="text-zinc-900 color-zinc-900 resize-none w-2/3 sm:w-5/6 h-10 ring-0 focus:outline-none rounded-full px-3 py-2"
           value={messageInput}
           onChange={(e) => setMessageInput(e.target.value)}
         />
         <button
-          className="text-blue-500 hover:text-blue-300 w-1/6 h-10"
+          className="text-blue-500 hover:text-blue-300 w-1/3 sm:w-1/6 h-10"
           type="submit"
-          onClick={(e) => {
-            if (messageInput !== "") {
-              handleSubmit(messageInput, user);
-            }
-          }}
-          onKeyDown={(e) => {
-            if (e.keyCode === 13 && !e.shiftKey && messageInput !== "") {
-              e.preventDefault();
-              handleSubmit(messageInput, user);
-            }
-          }}
         >
           Send
         </button>
